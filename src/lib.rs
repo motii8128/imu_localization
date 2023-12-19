ee15f97a483ab778019a10fdd325ee732644178f
motii <108280115+motii8128@users.noreply.github.com>
2023-12-19 16:00:43 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -79,10 +79,52 @@ fn observation_noise(rate:f64)->na::Matrix3<f64>
 fn predict_jacob(posture:na::Vector3<f64>, angular_velocity:na::Vector3<f64>, delta_time:f64)->na::Matrix3<f64>
 {
     let sin_x = (posture.x).sin();
+    let sin_y = (posture.y).sin();
     let cos_x = (posture.x).cos();
     let cos_y = (posture.y).cos();
     let tan_y = (posture.y).tan();
 
     let x_x = 1.0 + (angular_velocity.x + angular_velocity.y*tan_y*cos_x + angular_velocity.z*tan_y*sin_x)*delta_time;
     let x_y = (angular_velocity.y*(sin_x/cos_y.powi(2)) + angular_velocity.z)*delta_time;
+    let x_z = 0.0;
+
+    let y_x = (-1.0*angular_velocity.y*sin_x - angular_velocity.z*cos_x)*delta_time;
+    let y_y = 1.0;
+    let y_z = 0.0;
+
+    let z_x = (angular_velocity.y*(cos_x/cos_y) - angular_velocity.z*(sin_x/cos_y))*delta_time;
+    let z_y = (angular_velocity.y*sin_x*(sin_y/cos_y.powi(2)) + angular_velocity.z*cos_x*(sin_y/cos_y.powi(2)))*delta_time;
+    let z_z = 1.0;
+
+    na::Matrix3::<f64>::new(
+        x_x, x_y, x_z,
+        y_x, y_y, y_z,
+        z_x, z_y, z_z
+    )
+}
+
+fn observation_jacob()->na::Matrix3<f64>
+{
+    na::Matrix3::<f64>::new(
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+    )
+}
+
+fn calc_predict_disp(disp:na::Matrix3<f64>, jacob:na::Matrix3<f64>, predict_noise:na::Matrix3<f64>)->na::Matrix3<f64>
+{
+    jacob * disp * jacob.transpose() + predict_noise
+}
+
+fn calc_observation_disp(predict_disp:na::Matrix3<f64>, obs_jacob:na::Matrix3<f64>, obs_noise:na::Matrix3<f64>)->na::Matrix3<f64>
+{
+    obs_jacob*predict_disp*obs_jacob.transpose() + obs_noise
+}
+
+fn calc_kalman_gain(predict_disp:na::Matrix3<f64>, obs_jacob:na::Matrix3<f64>, obs_disp:na::Matrix3<f64>)->na::Matrix3<f64>
+{
+    let tv_obs_disp = obs_disp.try_inverse().unwrap();
+
+    predict_disp*obs_jacob.transpose()*tv_obs_disp
 }
